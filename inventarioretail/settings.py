2eafb91920c5eb0c2a09ba4242778cb98605fdc8
emoncada002 <e.moncada002@ebc.edu.mx>
2026-08-20 21:16:09 -0600

    actualización

--- inventarioretail/settings.py
+++ inventarioretail/settings.py
@@ -1,1 +1,17 @@
-# Configuración con el nuevo nombre del proyecto
+# Busca esta línea (aproximadamente en la línea 70) y cámbiala a:
+ROOT_URLCONF = 'inventarioretail.urls'
+
+# Busca esta línea (aproximadamente en la línea 110) y cámbiala a:
+WSGI_APPLICATION = 'inventarioretail.wsgi.application'
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Tu aplicación de gestión de inventario retail vinculada
+    'articulos',
+]
--- inventarioretail/urls.py
+++ inventarioretail/urls.py
@@ -1,1 +1,12 @@
-# Enrutador principal de artículos
+from django.contrib import admin
+from django.urls import path
+from articulos import views
+
+urlpatterns = [
+    path(
+        'admin/',
+        admin.site.split if hasattr(admin.site, 'split') else admin.site.urls,
+    ),
+    # Pantalla principal del inventario
+    path('', views.dashboard_retail, name='dashboard'),
+]
