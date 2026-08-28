4abdae66db252bd6a6536a79d617c4b8a4819623
SergioSalcedo21 <sa.salcedo2115@gmail.com>
2026-08-28 17:00:51 -0600

    Actualizar views de articulos

--- articulos/views.py
+++ articulos/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from articulos.dao.inventariodao import InventarioDAO
 from articulos.models import Producto, Categoria
+from django.contrib.auth.decorators import login_required, user_passes_test
 
+#Roles
+
+def es_admin(user):
+    """Verifica si el usuario autenticado pertenece al grupo 'Administración' o es staff"""
+    return user.is_authenticated and (user.groups.filter(name='Administración').exists() or user.is_staff)
+    #return user.is_authenticated and (user.groups.filter(name='Cocina').exists() or user.is_staff)
+
+@login_required
+@user_passes_test(es_admin, login_url='/admin/login/')
 
 @csrf_exempt
 def dashboard_retail(request):
